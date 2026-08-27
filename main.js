fcb231bb2f8b4a59064a6b3ee96932cf8ca50815
pietrokauan <pietro.kauan.santos@escola.pr.gov.br>
2026-08-27 12:44:11 -0300

    alterei os botoes

--- main.js
+++ main.js
@@ -1,2 +1,35 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto')
-numeroSenha.textContent = 5;
+const campoSenha = document.querySelector('#campo-senha')
+
+campoSenha.value = 'aqui vai aparecer a senha'
+
+let letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+let letrasMinusculas ='abcdefghijklmnopqrstuvwxyz'
+
+let Numeros ='0123456789'
+
+let 
+
+tamanhoSenha = 8
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll(".parametro-senha__botao")
+
+botoes[0].onclick = diminuir;
+
+function diminuir() {
+
+    tamanhoSenha--;
+    numeroSenha.textContent = tamanhoSenha
+    
+}
+
+botoes[1].onclick = aumentar;
+
+function aumentar() {
+
+    tamanhoSenha++;
+    numeroSenha.textContent = tamanhoSenha
+    
+}
